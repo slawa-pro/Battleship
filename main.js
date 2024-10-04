59ae56bc2b1d0fb0d170e7d5208486fc02502803
Vyacheslav <va.povhleb@gmail.com>
2024-10-05 00:07:42 +0300

    refactor: code (Fri, Oct 05, 2024 00:07:04)

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 let colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'gray', 'cyan'];
 let colorPairs = [...colors, ...colors];
-
-let container = document.getElementById('container');
 let selectedCells = [];
 
-colorPairs = colorPairs.sort(() => 0.5 - Math.random());
-
 const gameContainer = document.querySelector('.game-playing');
+
+
+function startNewGame() {
+  colorPairs = colorPairs.sort(() => 0.5 - Math.random());
+  
+  gameContainer.innerHTML = '';
+
   colorPairs.forEach(color => {
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -15,44 +18,68 @@ const gameContainer = document.querySelector('.game-playing');
     gameContainer.appendChild(cell);
   });
 
-let boxes = document.querySelectorAll('.cell');  
+  let boxes = document.querySelectorAll('.cell');  
 
-boxes.forEach(box => {
-  box.addEventListener('click', function() {
-    let color = box.getAttribute('data-color');
-    selectedCells.push(box);
+  boxes.forEach(box => {
+    box.addEventListener('click', function() {
+      let color = box.getAttribute('data-color');
+      selectedCells.push(box);
 
-    if (selectedCells.length === 2) {
-      compareColors(selectedCells);
-      selectedCells = []; 
-    }
-    console.log(color);
+      if (selectedCells.length === 2) {
+        setTimeout(() => {
+          compareColors(selectedCells);
+          selectedCells = [];
+        }, 100);
+      }
+
+      console.log(color);
+    });
   });
-});
-
+}
 
 function compareColors(cells) {
   let color1 = cells[0].getAttribute('data-color');
   let color2 = cells[1].getAttribute('data-color');
 
   if (color1 === color2) {
-    cells[0].classList.add('hidden');
-    cells[1].classList.add('hidden');
 
-    checkGameEnd(); 
+    cells[0].style.visibility = 'hidden';
+    cells[1].style.visibility = 'hidden';
+
+    checkGameEnd();
   } else {
-    console.log(color1, color2);
+    console.log('Цвета не совпадают: ', color1, color2);
   }
 }
 
 function checkGameEnd() {
-  const visibleCells = document.querySelectorAll('.cell:not(.hidden)');
-  
-  if (visibleCells.length === 0) {
-      displayWinningMessage();
+  const visibleCells = document.querySelectorAll('.cell');
+  let allHidden = true;
+
+  visibleCells.forEach(cell => {
+    if (cell.style.visibility !== 'hidden') {
+      allHidden = false;
+    }
+  });
+
+  if (allHidden) {
+    displayWinningMessage();
   }
 }
 
 function displayWinningMessage() {
-  
+  const winningMessage = document.getElementById('winningMessage');
+  winningMessage.classList.remove('hidden');
 }
+
+document.getElementById('restartGame').addEventListener('click', () => {
+  const winningMessage = document.getElementById('winningMessage');
+  winningMessage.classList.add('hidden');
+  startNewGame(); 
+});
+
+document.getElementById('startGameButton').addEventListener('click', () => {
+  const gameStartModal = document.getElementById('gameStartModal');
+  gameStartModal.classList.add('hidden');
+  startNewGame();
+});
