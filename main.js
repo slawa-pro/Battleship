cf163a011fc34030105d0ccdb048f96997873424
Vyacheslav <va.povhleb@gmail.com>
2024-10-05 18:15:46 +0300

    feat: sound and svg (Sut, Oct 05, 2024 18:15:04)

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 let colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'gray', 'cyan'];
 let colorPairs = [...colors, ...colors];
+
 let selectedCells = [];
+
+let startTime;
+let timerInterval; 
+let attempts = 0;
 
 const gameContainer = document.querySelector('.game-playing');
 
 function startNewGame() {
   colorPairs = colorPairs.sort(() => 0.5 - Math.random());
+  gameRestart.play();
   
+  const gameInfo = document.getElementById('gameInfo');
+  gameInfo.classList.remove('hidden');
+
   gameContainer.innerHTML = '';
 
   colorPairs.forEach(color => {
@@ -17,20 +26,28 @@ function startNewGame() {
     gameContainer.appendChild(cell);
   });
 
+  attempts = 0;
+  updateAttempts();
+  startTime = new Date();
+  startTimer();
+  
+
   let boxes = document.querySelectorAll('.cell');  
 
   boxes.forEach(box => {
     box.addEventListener('click', function() {
       let color = box.getAttribute('data-color');
       selectedCells.push(box);
+      clickSound.play();
 
       if (selectedCells.length === 2) {
+        attempts++;
+        updateAttempts();
         setTimeout(() => {
           compareColors(selectedCells);
           selectedCells = [];
         }, 100);
       }
-
       console.log(color);
     });
   });
@@ -51,6 +68,28 @@ function compareColors(cells) {
   }
 }
 
+function updateAttempts() {
+  document.getElementById('attempts-count').innerText = `Attempts: ${attempts}`;
+  if (attempts === 13) {
+    gameoverSound.play();
+    clearInterval(timerInterval); 
+    disableCells();
+    displayGameOverMessage();
+  }
+}
+
+function startTimer() {
+  const timeDisplay = document.getElementById('time-spent');
+  
+  function updateTime() {
+    let currentTime = new Date();
+    let timeSpent = Math.floor((currentTime - startTime) / 1000);
+    timeDisplay.innerText = `Time spent: ${timeSpent} sec`;
+  }
+
+  timerInterval = setInterval(updateTime, 1000);
+}
+
 function checkGameEnd() {
   const visibleCells = document.querySelectorAll('.cell');
   let allHidden = true;
@@ -62,6 +101,8 @@ function checkGameEnd() {
   });
 
   if (allHidden) {
+    clearInterval(timerInterval); 
+    winSound.play();
     displayWinningMessage();
   }
 }
@@ -71,9 +112,27 @@ function displayWinningMessage() {
   winningMessage.classList.remove('hidden');
 }
 
+function displayGameOverMessage() {
+  const gameOverMessage = document.getElementById('gameOverMessage');
+  gameOverMessage.classList.remove('hidden');
+}
+
+function disableCells() {
+  let boxes = document.querySelectorAll('.cell');
+  boxes.forEach(box => {
+    box.style.pointerEvents = 'none';
+  });
+}
+
 document.getElementById('restartGame').addEventListener('click', () => {
   const winningMessage = document.getElementById('winningMessage');
   winningMessage.classList.add('hidden');
+  startNewGame();
+});
+
+document.getElementById('restartGameOver').addEventListener('click', () => {
+  const gameOverMessage = document.getElementById('gameOverMessage');
+  gameOverMessage.classList.add('hidden');
   startNewGame(); 
 });
 
@@ -82,3 +141,20 @@ document.getElementById('startGameButton').addEventListener('click', () => {
   gameStartModal.classList.add('hidden');
   startNewGame();
 });
+
+//sound for game.
+const startGameButton = document.getElementById('startGameButton');
+const startSound = document.getElementById('start-sound');
+
+const clickSound = document.getElementById('click-sound');
+const winSound = document.getElementById('win-sound');
+const gameoverSound = document.getElementById('gameover-sound');
+const gameRestart = document.getElementById('game-restart');
+
+startGameButton.addEventListener('click', function() {
+  startSound.play();
+  startNewGame();
+});
+
+
+
